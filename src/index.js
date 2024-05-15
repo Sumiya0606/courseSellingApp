dafb5289524bae41a41349742566b8f040bb0b8e
Sumiya <sumithaju143@gmail.com>
2024-05-15 07:06:44 +0400

    second coomit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ const app = express()
 import cookieParser from "cookie-parser"
 import connectDb from "../config/db.js"
 import userRouter from "../routes/userRouter.js"
+import instructorRouter from "../routes/instructorRouter.js"
 
 
 const port = 3000
 app.use(express.json())
 app.use(cookieParser());
 app.use("/api/v1/user",userRouter)
+app.use("/api/v1/instructor",instructorRouter)
 connectDb();
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!')  
 })
 
 app.listen(port, () => {
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,3 +8,8 @@ const generateToken=(email)=>{
     return jsonwebToken.sign({data:email},secretKey,{expiresIn :"1d"});
 }
 export default generateToken;
+export const adminToken = (user) => {
+    return jsonwebToken.sign({ data: user.id, role: user.role }, secretKey, {
+      expiresIn: "1d",
+    });
+  };
